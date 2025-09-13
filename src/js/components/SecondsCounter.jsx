377e9f76f2a12f9d54f8f7a1a53b147c098f7b30
Seeker90 <chris.carver90@icloud.com>
2025-09-13 01:22:04 +0000

    background

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -3,17 +3,17 @@ const SecondsCounter = ({thousandsDigit, hundredsDigit, tensDigit, onesDigit}) =
 
     return (
         <>
-        <div className="counter-display d-flex mt-5 justify-content-center fs-1">
-        <div className="clock-image">
+        <div className="counter-display d-flex mt-5 justify-content-center fs-1 ">
+        <div className="clock-image text-light bg-black fs-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clock" viewBox="0 0 16 16">
             <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
             <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
             </svg>
         </div>
-        <div className="seconds thousands">{thousandsDigit}</div>
-        <div className="seconds hundreds">{hundredsDigit}</div>
-        <div className="seconds tens">{tensDigit}</div>
-        <div className="seconds ones">{onesDigit}</div>
+        <div className="seconds thousands text-light bg-black ">{thousandsDigit}</div>
+        <div className="seconds hundreds text-light bg-black ">{hundredsDigit}</div>
+        <div className="seconds tens text-light bg-black ">{tensDigit}</div>
+        <div className="seconds ones text-light bg-black ">{onesDigit}</div>
         </div>
         </>
     )
